cc43d3d48344504fd68f3712cc1eb0ce5a9b19f7
Sickle41 <dracopest@gmail.com>
2026-06-01 09:24:48 -0500

    DogFetch

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,20 @@ fetch('https://randomfox.ca/floof')
             foxImgElement.src = convertedData.image
         }
     )
+
+// Define an asynchronous function
+const getTheDog = async () => {
+    // Request the data and wait for the data to come back over the Web
+    const response = await fetch("https://random.dog/woof.json")
+
+    // Parse the received JSON string into an Object
+    const dogJSONConvertedToObject = await response.json()
+
+    // Target the HTML image element
+    const dogImgElement = document.querySelector("#dog")
+
+    // Update the `src` with a URL. The browser will request that image.
+    dogImgElement.src = dogJSONConvertedToObject.url
+}
+
+getTheDog()
